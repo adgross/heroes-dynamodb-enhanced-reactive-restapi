952aaf5bf4cf0cafa3ea5657645b86801a532fe8
Ádler Jonas Gross <adlerjgross@gmail.com>
2021-03-19 18:00:59 -0300

    First commit

---
+++ src/test/java/io/github/adgross/heroes/HeroesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.adgross.heroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

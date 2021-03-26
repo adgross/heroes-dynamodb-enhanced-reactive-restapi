03bc5ce4713a4a7a322bb57ddaf86f73f3a560dc
Ádler Jonas Gross <adlerjgross@gmail.com>
2021-03-26 03:33:52 -0300

    service: add service implementation

---
+++ src/main/java/io/github/adgross/heroes/service/HeroServiceImp.java
@@ -0,0 +1,50 @@
+package io.github.adgross.heroes.service;
+
+import io.github.adgross.heroes.model.Hero;
+import io.github.adgross.heroes.repository.HeroRepository;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class HeroServiceImp implements HeroService {
+
+  @Autowired
+  HeroRepository heroRepository;
+
+  public Flux<Hero> listHeroes() {
+    return heroRepository.getAll();
+  }
+
+  public Mono<Hero> findById(String id) {
+    return heroRepository.get(id);
+  }
+
+  public Mono<Hero> create(Hero hero) {
+    Hero newHero = new Hero();
+    newHero.setId(UUID.randomUUID().toString());
+    newHero.setName(hero.getName());
+    newHero.setUniverse(hero.getUniverse());
+    newHero.setFilms(hero.getFilms());
+
+    return heroRepository.put(newHero)
+        .then(Mono.just(newHero));
+  }
+
+  public Mono<Hero> update(String id, Hero hero) {
+    Hero updated = new Hero();
+    updated.setId(id);
+    updated.setName(hero.getName());
+    updated.setUniverse(hero.getUniverse());
+    updated.setFilms(hero.getFilms());
+
+    return heroRepository.update(updated);
+  }
+
+  public Mono<Hero> deleteById(String id) {
+    return heroRepository.delete(id);
+  }
+
+}
